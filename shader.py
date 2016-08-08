6e0434f124657a8328764a968f24069bc4b99a38
buseca <ruggero.motta@gmail.com>
2016-08-09 00:01:11 +0200

    first commit
    creating the repo

---
+++ shader.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3.
+
+#this script is part of a project created during the 'Hackers & Designer' 2016
+
+#let's open an url with python
+import webbrowser
+webbrowser.open('http://inventwithpython.com/')
+
+#let's make it sleep
+import os
+os.system("pmset sleepnow")
---
+++ shader.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#this script is part of a project created during the 'Hackers & Designer' 2016
+
+while true
+do
+	read a; echo $a;
+	if [ $a == "s" ]; then
+		pmset sleepnow
+	fi
+	if [ $a == "b" ]; then
+		open "http://thatsthefinger.com/"
+	fi
+	if [ $a == "w" ]; then
+		echo "if you are so smart why you are so poor?" >> HeD.txt
+		open -e HeD.txt
+	fi
+	# call the python script, if you prefer to use python.
+	# if [ $a == "0" ]; then
+	# 	python /Users/ruggero/Desktop/HeDscript.py
+	# fi
+done
